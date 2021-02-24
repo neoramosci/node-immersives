1642fe6103b7e0328e8ce5d837d23e2ed8543abc
Irineo Ramos <irineo.ramos@codeimmersives.com>
2021-02-24 09:53:38 -0600

    all nodes pass.

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -4,5 +4,10 @@ const getInput = require("./get-input.js");
 
 const fName = getInput(0);
 const lName = getInput(1);
-const eMail = "@codeimmersives.com";
-console.log(fName + '.' + lName + eMail);
+const fullName = fName + " " + lName;
+const greet = "You are now in the matrix"
+const eMail = fName.toLowerCase() + '.' + lName.toLowerCase() +"@codeimmersives.com";
+console.log('Hi, ' + fName + "!");
+console.log('Your name capitalized is: ' + fName.toUpperCase());
+console.log('Your initials are : ' + fName[0] + '.' + lName[0] + ".");
+console.log('Your code Immersives email address is ' + eMail);
